3d36c8e43d0fbe5b1040225ddac94f44f2c14cc6
igor2354 <66739722+igor2354@users.noreply.github.com>
2021-05-05 13:52:13 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,39 +1,78 @@
 document.addEventListener(
 	"DOMContentLoaded",
 	function () {
-        const sliderRecomend = new Swiper('.slider-recommend', {
-            slidesPerView: 1,
-            spaceBetween: 10,
-            navigation: {
-                nextEl: '.slider-recommend__next',
-                prevEl: '.slider-recommend__prev',
-            },
+		const sliderRecomend = new Swiper(".slider-recommend", {
+			slidesPerView: "auto",
+			spaceBetween: 10,
 
-            breakpoints: {
-                500: {
-                    slidesPerView: 2,
-                    spaceBetween: 10,
-                  },
+			// slidesPerView: 1,
+			// spaceBetween: 10,
+			navigation: {
+				nextEl: ".slider-recommend__next",
+				prevEl: ".slider-recommend__prev",
+			},
 
-                768: {
-                  slidesPerView: "auto",
-                  spaceBetween: 10,
-                },
-              }
-        });
-    },
+			// breakpoints: {
+			// 	500: {
+			// 		slidesPerView: 2,
+			// 		spaceBetween: 10,
+			// 	},
+
+			// 	768: {
+			// 		slidesPerView: "auto",
+			// 		spaceBetween: 10,
+			// 	},
+			// },
+		});
+	},
 	false
 );
 
 $(document).ready(function () {
-    $(".products-promocode__name").on("click", function() {
-        if ($(this).parent().hasClass("active")) {
-            $(this).parent().removeClass("active");
-            $(this).next().slideUp();
-        } else {
-            $(this).parent().addClass("active");
-            $(this).next().slideDown();
-        }
+	$(".products-promocode__name").on("click", function () {
+		if ($(this).parent().hasClass("active")) {
+			$(this).parent().removeClass("active");
+			$(this).next().slideUp();
+		} else {
+			$(this).parent().addClass("active");
+			$(this).next().slideDown();
+		}
+	});
 
-    });
+	let match = window.matchMedia("(max-width: 768px)");
+
+	function fixedButton() {
+		if (match.matches) {
+			$(window).on("resize, load", function () {
+				let elHeight = $(".basket-products__list").height();
+				let windowHeight = $(window).height();
+
+				if (elHeight > windowHeight) {
+					$(".products-result__button").addClass("fixed");
+
+					$(window).on("scroll", function () {
+						$(window).scroll(function () {
+							var wt = $(window).scrollTop();
+							var wh = $(window).height();
+							var et = $(".products-result").offset().top;
+							var eh = $(".products-result").outerHeight();
+							var dh = $(document).height();
+							if (wt + wh >= et || wh + wt == dh || eh + et < wh) {
+								$(".js-button-order").removeClass("fixed");
+							} else {
+								$(".js-button-order").addClass("fixed");
+							}
+						});
+					});
+				} else {
+					$(".js-button-order").removeClass("fixed");
+				}
+			});
+		} else {
+			$(".js-button-order").removeClass("fixed");
+		}
+	}
+
+	fixedButton();
+	match.addListener(fixedButton);
 });
